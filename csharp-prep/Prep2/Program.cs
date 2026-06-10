b753607c9454250d82b5c5053fb6f3b9ba19aa72
alexgt95 <40178272+alexgt95@users.noreply.github.com>
2026-06-10 14:18:18 -0600

    W01 Prove Programming parts 1-2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What percentage grade did you get? ");
+        string userInput = Console.ReadLine();
+        int percentageGrade = int.Parse(userInput);
+        string letterGrade;
+
+    if (percentageGrade < 0 || percentageGrade > 100)
+        {
+            letterGrade = "That is not a valid grade percentage";
+        }
+        else if (percentageGrade >= 90 && percentageGrade <= 100)
+        {
+            letterGrade = "A";
+        }
+        else if (percentageGrade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percentageGrade >= 80)
+        {
+            letterGrade = "C";
+        }
+        else if (percentageGrade >= 80)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}");
+
+        if (percentageGrade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+
     }
 }
