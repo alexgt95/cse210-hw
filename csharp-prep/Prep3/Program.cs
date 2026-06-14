22668a3595da8af5d3bd76a2d1e6e051b123f8bc
alexgt95 <40178272+alexgt95@users.noreply.github.com>
2026-06-13 20:46:36 -0600

    W01 Prove: Developer—C# Programming Parts 3–5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
+
+/*
+WHY NOT A DO WHILE? COULD I HAVE GUESS BE PART OF THE SCOPE?
+ using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int magicNumber = 5;
+        int guess = -1;
+
+        do
+        {
+            Console.Write("What is the magic number? ");
+            string userInput = Console.ReadLine();
+            int guess = int.Parse(userInput);
+        } while (guess != magicNumber);
+    }
+}
+*/
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+
+        string userFullName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+
+        int squaredValue = SquareNumber(favoriteNumber);
+
+        int yearOfBirth;
+        PromptUserBirthYear(out yearOfBirth);
+
+
+        DisplayResult(userFullName, squaredValue, yearOfBirth);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int enteredNumber = int.Parse(Console.ReadLine());
+
+        return enteredNumber;
+    }
+    
+    static void PromptUserBirthYear(out int yearOfBirth)
+    {
+        Console.Write($"Please enter the year you were born: ");
+        yearOfBirth = int.Parse(Console.ReadLine());
+
+    }
+
+    static int SquareNumber(int number)
+    {
+        int squaredResult = number * number;
+        return squaredResult;
+    }
+
+    static void DisplayResult(string userName, int squaredValue, int yearOfBirth)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredValue}.");
+        Console.WriteLine($"{userName}, you will turn {2025 - yearOfBirth} years old this year.");
     }
 }
